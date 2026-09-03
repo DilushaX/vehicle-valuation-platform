cbaef66f3facfc6373abb4c64177c9ae56dd8248
Dilusha Rajapaksha <dilusharajapaksha2005@gmail.com>
2026-09-03 13:17:50 +0530

    chore: Recreate all

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,12 +1,1 @@
-"""Scraper module initialization."""
-from scraper.client import PoliteHttpClient
-from scraper.parsers.riyasewana_parser import RiyasewanaParser
-from scraper.spiders.riyasewana_spider import RiyasewanaSpider
-from scraper.mock_data_generator import generate_realistic_dataset
-
-__all__ = [
-    "PoliteHttpClient",
-    "RiyasewanaParser",
-    "RiyasewanaSpider",
-    "generate_realistic_dataset"
-]
+"""Scraper package for vehicle listing data collection."""
